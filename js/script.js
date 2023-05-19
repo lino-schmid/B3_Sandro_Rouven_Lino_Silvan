44781605d30f8c5111f97c3c4306cdc1ac99d782
lino-schmid <132079199+lino-schmid@users.noreply.github.com>
2023-05-19 17:10:18 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 // Ebenfalls vom Hamburger Tutorial
 // https://www.youtube.com/watch?v=flItyHiDm7E 
 
-const hamburger = document.getElementsByClassName("hamburger");
-const navItems = document.getElementsByClassName("nav-items");
+const hamburger = document.getElementById("hamburger");
+const navItems = document.getElementById("nav-items");
+const body = document.getElementById("body");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navItems.classList.toggle("active");
+    body.classList.toggle("active");
 });
